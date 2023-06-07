178aa3860873ed789d3a3b9051043684fc1c72e3
k-ibaraki <ibaraki@ncdc.co.jp>
2023-06-07 20:05:48 +0900

    slsが特定のテンプレ前提になっていたので、シンプルな構造に修正

--- sls/ibaraki-sls-sample-api/serverless.ts
+++ sls/ibaraki-sls-sample-api/serverless.ts
@@ -1,27 +1,34 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
   service: 'ibaraki-sls-sample-api',
   frameworkVersion: '3',
   plugins: ['serverless-esbuild'],
+  package: {
+    individually: true,
+  },
   provider: {
     name: 'aws',
     runtime: 'nodejs18.x',
     region: 'ap-northeast-1',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
   },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
+  functions: {
+    hello: {
+      handler: 'ibaraki-sample-lambda/src/index.handler',
+      events: [
+        {
+          http: {
+            path: 'hello',
+            method: 'get'
+          }
+        }
+      ]
+    }
+  },
   custom: {
     esbuild: {
       bundle: true,
